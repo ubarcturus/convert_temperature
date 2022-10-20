657c8dc90551b4ba17b29354ce61bca9272e21ca
kuki <ubarcturus@gmail.com>
2022-10-20 11:05:43 +0200

    add input for unit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut unit = String::new();
+
+    println!("What is the desired temperature unit? Enter Celsius or Fahrenheit.");
+
+    unit = read_unit();
+
+    println!("You entered {unit}.");
+}
+
+fn read_unit() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    return input.trim().to_string();
 }
